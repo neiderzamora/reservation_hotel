a73983d34b83a4adfc90e431f9b6f8cd088823bf
ihuntersouls <neiderzamoramoreno@gmail.com>
2023-05-28 16:43:05 -0500

    crud 2.0 reservas

--- Frontend/src/routes/AppRoutes.jsx
+++ Frontend/src/routes/AppRoutes.jsx
@@ -5,6 +5,7 @@ import Page_not_found from "../pages/Page_not_found";
 import Habitaciones from "../pages/Habitaciones";
 import EditReserve from "../pages/EditReserve";
 import Reserva from "../pages/Reserva";
+import AddHabit from "../pages/AddHabit";
 
 const AppRoutes = () => {
     return (
@@ -13,7 +14,10 @@ const AppRoutes = () => {
           <Route path="/panel" element={<Panel />} />
           <Route path="/" element={<Habitaciones />} />
           <Route path="/reservar" element={<Reserva />} />
-          
+          <Route path="/add" element={<AddHabit />} />
+          <Route path="/editar-reserva/:id" element={<EditReserve />} /> {/* agregar el id cuando se consuma datos del back */}
+          {/* <Route path="/habitaciones" element={<Habitaciones />} /> */}
+          <Route path="/*" element={<Page_not_found />} />
         </Routes>
       </>
     );
